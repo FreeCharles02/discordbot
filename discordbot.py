6530d67b9112b763ca3c059825054db3b3894aba
Charles <105826694+FreeCharles02@users.noreply.github.com>
2025-05-23 11:15:27 -0500

    dale

--- discordbot.py
+++ discordbot.py
@@ -20,8 +20,8 @@ async def on_message(message):
         await message.channel.send('https://cdn.discordapp.com/attachments/879979824167407646/1289527288471490590/2024-09-28_10-08-14.mp4?ex=6830475f&is=682ef5df&hm=37c632344b7f758a245930e0c725986401cf76fccd02813f428d7aac28626e88&')
     if message.content.startswith('$hello'):
         await message.channel.send('What\'s up MEXICAN!')
-    if "dale" in message.contentlower(): 
-        await message.channel.send()
+    if "dale" in message.content.lower(): 
+        await message.channel.send('https://cdn.discordapp.com/attachments/1173495961419522081/1375507194107133973/CLIP_LowTierGod_Gets_Heated_By_Being_Called_His_Government_Name.mp4?ex=6831f066&is=68309ee6&hm=cd74a48008e74ff716cb6add27fa04e16d75cf171d8bdca9c5e1f7aafde3cfad&')
     if "kaguya" in message.content.lower() or "coal" in message.content.lower(): 
         await message.channel.send('https://cdn.discordapp.com/attachments/1173495961419522081/1374992539676839956/niggas_who_say_hood_weeb_on_twitter_summarized.mp4?ex=68301117&is=682ebf97&hm=2b1bf31b6ddc7470efc45a0c5b1b4c56a89f8c7479e9d2cf78600167b52b6462&')
 
